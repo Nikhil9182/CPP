66c8be4d3747b76d60ef481698345e8db8c0a815
Nikhil Bansal <64183769+Nikhil9182@users.noreply.github.com>
2022-09-05 01:53:52 +0530

    Time: 109 ms (83.24%), Space: 59 MB (36.50%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -4,7 +4,7 @@ public:
         int left = 0;
         int right = height.size() - 1;
         int water = 0;
-        while(left <= right)
+        while(left < right)
         {
             water = max(water,(right-left) * min(height[left],height[right]));
             (height[left] < height[right])?left++:right--;
