5a94da9d1eda8caeb65092a3af6db147634438b0
Nikhil Bansal <64183769+Nikhil9182@users.noreply.github.com>
2022-09-05 02:33:36 +0530

    Time: 300 ms (46.93%), Space: 121.3 MB (29.48%) - LeetHub

--- 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
+++ 2216-minimum-deletions-to-make-array-beautiful/2216-minimum-deletions-to-make-array-beautiful.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int minDeletion(vector<int>& nums) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         int ans = 0, n  = nums.size();
         for(int i = 0; i < n-1; i++) if(nums[i]==nums[i+1] && (i-ans)%2==0) ans++;
         return ans+(n-ans)%2;
